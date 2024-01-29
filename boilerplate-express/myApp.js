7abac71dfa29c52b4423fefe0326909c8fdf8f8a
samoyla <samoyla@hotmail.com>
2024-01-29 18:05:35 +0100

    query, middleware ...

--- boilerplate-express/myApp.js
+++ boilerplate-express/myApp.js
@@ -1,7 +1,65 @@
+require('dotenv').config();
+
 let express = require('express');
 let app = express();
 
 
+console.log("Hello World");
+
+// Middleware function to add current time to req.time
+const addCurrentTime = (req, res, next) => {
+    req.time = new Date().toString();
+    next(); // Call next to move to the final handler
+};
+
+// Route with the middleware and the final handler
+app.get('/now', addCurrentTime, (req, res) => {
+    // Respond with a JSON object containing the current time
+    res.json({ time: req.time });
+});
+
+app.use((req, res, next) =>{
+    // const logString =  req.method + " " + req.path + " - " + req.ip;
+    const logString = `${req.method} ${req.path} - ${req.ip}`;
+    console.log(logString);
+    next();
+});
+
+app.use("/public", express.static(__dirname + "/public"));
+
+app.get("/", (req, res) =>{
+    //res.send('Hello Express');
+    let absolutePath = __dirname + "/views/index.html";
+    res.sendFile(absolutePath);
+});
+
+app.get("/json", (req, res) => {
+    
+    const messageStyle = process.env.MESSAGE_STYLE;
+    let message = "Hello json";
+    if (messageStyle === "uppercase") {
+        message = message.toUpperCase();
+    }
+        res.json({
+            message: message
+        });
+});
+
+app.get("/:word/echo", (req, res) => {
+    const {word} = req.params;
+    res.json({
+        echo:word
+    });
+});
+
+app.get("/name", (req, res)=>{
+    let first = req.query.first;
+    let second = req.query.last;
+    // let {first: first, last: second} = req.query;
+    res.json({
+        name:`${first} ${second}`
+    });
+});
 
 
 
@@ -31,9 +89,4 @@ let app = express();
 
 
 
-
-
-
-
-
- module.exports = app;
+module.exports = app;
